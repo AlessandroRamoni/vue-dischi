f3423fab7aac6b14df4e0b5b5bc7b2dbb6d68c93
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-26 15:55:01 +0200

    import API

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,27 @@
 </template>
 
 <script>
+import axios from "axios";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 
 export default {
   name: "App",
-  // data(){
-  //   return {
-  //     indirizzoApi: "https://flynn.boolean.careers/exercises/api/array/music",
-  //   }
-  // }
+  data() {
+    return {
+      indirizzoApi: "https://flynn.boolean.careers/exercises/api/array/",
+      arrayAppoggio: [],
+    };
+  },
+  created() {
+    console.log("caricamento dati");
+    axios.get(this.indirizzoApi + "music").then(({ status, data }) => {
+      if (status === 200) {
+        this.arrayAppoggio = data;
+      }
+      console.log(data);
+    });
+  },
+
   components: {
     HeaderComponent,
   },
@@ -22,4 +34,8 @@ export default {
 
 <style lang="scss">
 @import "./assets/style/generics.scss";
+#app {
+  height: calc(100vh);
+  background-color: #1e2d3b;
+}
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+header {
+  background-color: #2e3a46;
+}
 img {
-  width: 80px;
+  width: 35px;
+  display: block;
+  border-radius: 50%;
+  margin-left: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
 }
 </style>
